69fa08f7f1de8352c09137ead3faf86c019799e6
yuliia.frund <yuliia.frund@uzh.ch>
2024-05-09 17:04:06 +0200

    fix train_bpe.sh (proper concatenation) and joint_vocab2k.txt and joint_vocab4k.txt

--- scripts/train_bpe.sh
+++ scripts/train_bpe.sh
@@ -10,14 +10,16 @@ trg=en
 
 #train with 2k vocabulary
 subword-nmt learn-joint-bpe-and-vocab --input $data/subsampled/train.$src $data/subsampled/train.$trg --total-symbols -s 2000 -o $data/codes2000.bpe --write-vocabulary $data/vocab2k.$src $data/vocab2k.$trg
-cat $data/vocab2k.$src $data/vocab2k.$trg > $data/joint_vocab2k.txt
+cat $data/vocab2k.$src >> $data/joint_vocab2k.txt
+cat $data/vocab2k.$trg >> $data/joint_vocab2k.txt
 
 subword-nmt apply-bpe -c $data/codes2000.bpe --vocabulary $data/vocab2k.$src --vocabulary-threshold 2000 < $data/subsampled/train.$src > $data/subsampled/train.2k.BPE.$src
 subword-nmt apply-bpe -c $data/codes2000.bpe --vocabulary $data/vocab2k.$trg --vocabulary-threshold 2000 < $data/subsampled/train.$trg > $data/subsampled/train.2k.BPE.$trg
 
-#train with 4k vocabulary
+##train with 4k vocabulary
 subword-nmt learn-joint-bpe-and-vocab --input $data/subsampled/train.$src $data/subsampled/train.$trg --total-symbols -s 4000 -o $data/codes4000.bpe --write-vocabulary $data/vocab4k.$src $data/vocab4k.$trg
-cat $data/vocab4k.$src $data/vocab4k.$trg > $data/joint_vocab4k.txt
+cat $data/vocab4k.$src >> $data/joint_vocab4k.txt
+cat $data/vocab4k.$trg >> $data/joint_vocab4k.txt
 
 subword-nmt apply-bpe -c $data/codes4000.bpe --vocabulary $data/vocab4k.$src --vocabulary-threshold 4000 < $data/subsampled/train.$src > $data/subsampled/train.4k.BPE.$src
 subword-nmt apply-bpe -c $data/codes4000.bpe --vocabulary $data/vocab4k.$trg --vocabulary-threshold 4000 < $data/subsampled/train.$trg > $data/subsampled/train.4k.BPE.$trg
