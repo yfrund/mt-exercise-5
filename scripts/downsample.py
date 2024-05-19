48e755c846baf04476a256350f20517e783d0947
yuliia.frund <yuliia.frund@uzh.ch>
2024-05-19 13:32:35 +0200

    modify downsample.py to accept command-line arguments

--- scripts/downsample.py
+++ scripts/downsample.py
@@ -1,5 +1,16 @@
+import argparse
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input", type=str, nargs="+", help="Path to vocabulary files whose frequencies need to be removed", required=True)
+    parser.add_argument("--output", type=str, nargs="+", help="Path to save clean vocabulary files", required=True)
+    parser.add_argument("--num_lines", type=int, help="Number of lines to sample", required=True)
+
+    args = parser.parse_args()
+
+    return args
 def sample(data, num_of_sents, out):
     """
     Saves a specified number of lines in a given directory
@@ -18,8 +29,13 @@ def sample(data, num_of_sents, out):
 
 
 def main():
-    sample('../data/train.nl-en.en', 100000, '../data/subsampled/train.en')
-    sample('../data/train.en-nl.nl', 100000, '../data/subsampled/train.nl')
+    # sample('../data/train.nl-en.en', 100000, '../data/subsampled/train.en')
+    # sample('../data/train.en-nl.nl', 100000, '../data/subsampled/train.nl')
+
+    args = parse_args()
+
+    for infile, outfile in zip(args.input, args.output):
+        sample(infile, args.num_lines, outfile)
 
 if __name__ == '__main__':
     main()
