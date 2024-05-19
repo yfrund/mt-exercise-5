05b498baad1298ceacb0bd60a435361f7d720082
yuliia.frund <yuliia.frund@uzh.ch>
2024-05-19 15:09:28 +0200

    update paths to data and save output to file

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -3,15 +3,16 @@
 scripts=$(dirname "$0")
 base=$scripts/..
 
-data=$base/sampled_data
+data=$base/data
 configs=$base/configs
+logs=$base/logs
 
 translations=$base/translations
 
 mkdir -p $translations
 
-src=?
-trg=?
+src=nl
+trg=en
 
 
 num_threads=4
@@ -21,7 +22,7 @@ device=0
 
 SECONDS=0
 
-model_name=?
+model_name=transformer_bpe1k
 
 echo "###############################################################################"
 echo "model_name $model_name"
@@ -30,11 +31,11 @@ translations_sub=$translations/$model_name
 
 mkdir -p $translations_sub
 
-CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/test.$src > $translations_sub/test.$model_name.$trg
+CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/dev.test/test.$src > $translations_sub/test.$model_name.$trg
 
 # compute case-sensitive BLEU 
 
-cat $translations_sub/test.$model_name.$trg | sacrebleu $data/test.$trg
+cat $translations_sub/test.$model_name.$trg | sacrebleu $data/dev.test/test.$trg > $logs/$model_name/$model_name.bleu.txt 2>&1
 
 
 echo "time taken:"
